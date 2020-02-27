54a279e5de3bb54017053cedd1d0db2d8d90afdf
veremeenkoMaksim <veremeenko@gmail.com>
2020-02-27 11:13:16 +0900

    WritedClass

---
+++ SUDS/SUDS/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUDS
+{
+    class Person
+    {
+        private string name { get; set; }
+        private string surname { get; set; }
+        private DateTime dateOfBirth { get; set;}
+    }
+}
---
+++ SUDS/SUDS/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUDS
+{
+    class User : Person
+    {
+        private string login { get; set; }
+        private string password { get; set; }
+        private int role { get; set; }
+        
+
+    }
+}
