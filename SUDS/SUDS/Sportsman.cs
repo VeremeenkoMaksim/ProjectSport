c6054532e503c8bf80bc040ec4c4c4648874a2ba
NEFUstudent <study.svfu@gmail.com>
2020-02-27 09:42:45 +0900

    create Class Sportsman

---
+++ SUDS/SUDS/Sportsman.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SUDS
+{
+    class Sportsman
+    {
+        [Key]
+        private int SportsmanId { get; set; }
+        private string name { get; set; }
+        private string surname {get; set;}
+        private string middleName { get; set;}
+    }
+}
